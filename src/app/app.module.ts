3a4f1cacba03daea644e5262d70444f8af48c1dd
HaoNguyen <itshaonguyen@gmail.com>
2024-01-27 00:55:27 +0700

    initial-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [HomeComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [HomeComponent],
 })
-export class AppModule { }
+export class AppModule {}
