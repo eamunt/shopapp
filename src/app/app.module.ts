736f31b770ac6cf06480dd0a83bb9988d52fe2c7
HaoNguyen <itshaonguyen@gmail.com>
2024-01-28 00:51:57 +0700

    order-page /var/folders/7h/4rtg7qqx3wj_fj110gz9vvnr0000gn/T/TemporaryItems/NSIRD_screencaptureui_xxY29q/Screen Shot 2024-01-28 at 00.51.51.png

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
-    declarations: [HomeComponent, HeaderComponent, FooterComponent],
+    declarations: [HomeComponent, HeaderComponent, FooterComponent, OrderComponent, DetailProductComponent],
     imports: [BrowserModule],
     providers: [],
-    bootstrap: [HomeComponent],
+    bootstrap: [
+        //HomeComponent
+        OrderComponent,
+    ],
 })
 export class AppModule {}
