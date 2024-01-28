2d12f59b030d59d05d7cb2c38d7103652d1dbc6a
HaoNguyen <itshaonguyen@gmail.com>
2024-01-28 13:36:26 +0700

    confirm-order-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OrderComponent } from './order/order.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 @NgModule({
-    declarations: [HomeComponent, HeaderComponent, FooterComponent, OrderComponent, DetailProductComponent],
+    declarations: [
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent,
+        OrderComponent,
+        DetailProductComponent,
+        OrderConfirmComponent,
+    ],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [
         //HomeComponent
-        OrderComponent,
+        // OrderComponent,
+        OrderConfirmComponent,
     ],
 })
 export class AppModule {}
