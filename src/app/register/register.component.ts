a904cef656b612b83c4e880086bf8746aa63909c
HaoNguyen <itshaonguyen@gmail.com>
2024-03-06 17:50:30 +0700

    feat(register): validate Angular Form with ngIf / check length / check retypePassword / check Age

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+    @ViewChild('registerForm') registerForm!: NgForm;
     // declare variables tương ứng với các fields trong form.
     phone: string;
     password: string;
@@ -38,5 +40,30 @@ export class RegisterComponent {
             `is accepted: ${this.isAccepted}` +
             `dateOfBirth: ${this.dateOfBirth}`;
         alert(message);
+    }
+
+    // check password matched ?
+    checkPasswordsMatch() {
+        if (this.password !== this.retypePassword) {
+            this.registerForm.form.controls['retypePassword'].setErrors({ passwordMismatch: true });
+        } else {
+            this.registerForm.form.controls['retypePassword'].setErrors(null);
+        }
+    }
+    checkAge() {
+        if (this.dateOfBirth) {
+            const today = new Date();
+            const birthDate = new Date(this.dateOfBirth);
+            let age = today.getFullYear() - birthDate.getFullYear();
+            const monthDiff = today.getMonth() - birthDate.getMonth();
+            if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+            }
+            if (age < 18) {
+                this.registerForm.form.controls['dateOfBirth'].setErrors({ invalidAge: true });
+            } else {
+                this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+            }
+        }
     }
 }
