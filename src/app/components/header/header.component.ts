46c0303162602ef04563c859fadf95ceba3e099b
haostudio <itshaonguyen@gmail.com>
2024-04-06 15:55:35 +0700

    refactor(): using ngClass to fill bg for nav-bar instead of get HTML element before

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,9 +11,10 @@ import { HomeComponent } from '../home/home.component';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+    [x: string]: any;
     userResponse?: UserResponse | null;
     isPopoverOpen = false;
-    static isClicked: number = 0;
+    static activeNavItem: number = 0;
     static currentPage: any;
 
     togglePopover(event: Event): void {
@@ -40,7 +41,6 @@ export class HeaderComponent implements OnInit {
             case 2:
                 // Xử lý khi click vào "Đăng xuất"
                 debugger;
-                HeaderComponent.isClicked = 3;
                 this.tokenService.logout();
                 break;
             default:
@@ -48,66 +48,27 @@ export class HeaderComponent implements OnInit {
         }
     }
 
-    fill_bg(): void {
-        let header = document.getElementById('navbarNav');
-        let btns = header!.getElementsByClassName('nav-link');
-        let current = document.getElementsByClassName('active');
-        if (current.length !== 0) {
-            current[0].className = current[0].className.replace(' active', '');
-            debugger;
-            btns[HeaderComponent.isClicked].className += ' active';
-        }
-    }
     ngOnInit() {
         this.userResponse = this.userService.getUserResponseFromLocalStorage();
-        HeaderComponent.currentPage = window.location;
-        if (HeaderComponent.currentPage.pathname === '/login') {
-            let current = document.getElementsByClassName('active');
-            if (current.length !== 0) {
-                current[0].className = current[0].className.replace(' active', '');
-                debugger;
-                let e = document.getElementById('loginTag');
-                if (e) {
-                    e!.className += ' active';
-                }
-            }
-        }
-        if (HeaderComponent.currentPage.pathname === '/orders') {
-            let current = document.getElementsByClassName('active');
-
-            HeaderComponent.isClicked = 2;
-            this.fill_bg();
-        }
         debugger;
-        if (this.userResponse) {
-            debugger;
-            this.fill_bg();
+        HeaderComponent.currentPage = window.location;
+
+        // fill bg for trang chủ when login.
+        if (this.userResponse && HeaderComponent.activeNavItem == -1) {
+            HeaderComponent.activeNavItem = 0;
+        }
+
+        if (!this.userResponse) {
+            if (HeaderComponent.currentPage.pathname === '/login') {
+                HeaderComponent.activeNavItem = -1;
+            }
         }
     }
     activeClick(index: number): void {
         debugger;
-        HeaderComponent.isClicked = index;
-        this.fill_bg();
+        HeaderComponent.activeNavItem = index;
     }
-
-    // ngAfterViewInit(): void {
-    //     console.log('hello');
-    //     // Add active class to the current button (highlight it)
-    //     let header = document.getElementById('navbarNav');
-    //     console.log(header);
-    //     debugger;
-    //     let btns = header!.getElementsByClassName('nav-link');
-    //     console.log('btns', btns);
-    //     for (let i = 0; i < btns.length; i++) {
-    //         console.log(`btns`, btns[i]);
-    //         btns[i].addEventListener('click', () => {
-    //             this.isClicked = i;
-    //             console.log('btns[i]', btns[i]);
-    //             debugger;
-    //             let current = document.getElementsByClassName('active');
-    //             current[0].className = current[0].className.replace(' active', '');
-    //             btns[this.isClicked].className += ' active';
-    //         });
-    //     }
-    // }
+    getActivateNavItem(): number {
+        return HeaderComponent.activeNavItem;
+    }
 }
